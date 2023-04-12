6ac66e3b00dae7e91a5b4b159b9ad5d08ecd48a6
NehaP0 <nehaphadtare334@gmail.com>
2023-04-12 14:14:25 +0530

    commit

--- NEM 111/Sprint 2/day 1/index.js
+++ NEM 111/Sprint 2/day 1/index.js
@@ -5,6 +5,13 @@ const fs = require("fs")
 
 app.use(express.json())
 
+app.get("/", (req,res)=>{ 
+
+    res.setHeader("content-type", "text/html")
+    res.write(`<h1>Home Page</h1>`)
+    res.end()
+})
+
 app.get("/students", (req,res)=>{
     // const data = JSON.parse(fs.readFileSync("./db.json", "utf-8"))
     // console.log(data)
@@ -13,45 +20,57 @@ app.get("/students", (req,res)=>{
     const studentStream = fs.createReadStream("./logs.txt", "utf-8")
     studentStream.pipe(res)
     //use autocannon to check
-
 })
+
 app.post("/adddata", (req, res)=>{
+    //In postman, select method "POST"
+    //url = http://localhost:3000/adddata
+    //Select -> Body, raw, JSON
+    //write data in json format, like {"name":"yeah"}
+    //Send
     const data = JSON.parse(fs.readFileSync("./db.json", "utf-8"))
-    data.students(req.body)
+    data.students.push(req.body)
     console.log(data)
-    fs.writeFile("./db.json", JSON.stringify(data))
+    fs.writeFileSync("./db.json", JSON.stringify(data))
     res.end("data is added")
 })
 
+                                        
 app.patch("/updatestudent", (req,res)=>{//we also need id which we will learn later
-    
+    //In postman, select method "PATCH"
+    //url = http://localhost:3000/updatestudent
+    //Select -> Body, raw, JSON
+    //write data in json format, like { "city" : "pune"}
+    //Send
     const data = JSON.parse(fs.readFileSync("./db.json", "utf-8"))
-    for(let i=0; i<data.students.length; i++){//can use for each also
+    for(let i=0; i<data.students.length; i++){//can use for-each also
         if(data.students[i].name=="Rahul"){
             data.students[i].city = req.body.city
         }
     }
-    fs.writeFile("./db.json", JSON.stringify(data))
+    fs.writeFileSync("./db.json", JSON.stringify(data))
     res.end("data is updated")
 })
 
+                            
 app.delete("/deletestudent", (req, res)=>{//we also need id which we will learn later
-
+    //In postman, select method "DELETE"
+    //url = http://localhost:3000/deletestudent
+    //Select -> none
+    //Send
     const data = JSON.parse(fs.readFileSync("./db.json", "utf-8"))
 
-    let newstuarray = data.students.filter((item)=>{
+    let newStuArray = data.students.filter((item)=>{
         return item.name!=="Rahul"
     })
 
-    data.students = newstuarray
+    data.students = newStuArray
 
-    fs.writeFile("./db.json", JSON.stringify(data))
+    fs.writeFileSync("./db.json", JSON.stringify(data))
     res.end("data is deleted")
 })
 
-// app.post("/adddata", (req, res)=>{
-//     console.log(req.body);
-//     res.send("data added")
-// })
 
-app.listen(8000)
+app.listen(3000,()=>{
+    console.log("server running at port 3000")
+})
