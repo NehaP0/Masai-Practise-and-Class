1190bbc6a3641fd9484fb81b384cfd81a1c7f367
Your Name <nehaphadtare334@gmail.com>
2023-05-02 06:41:20 +0530

    commit

--- NEM 111/Sprint 4/day 3 Full Stack CRUD 2/frontend/src/components/Notes.jsx
+++ NEM 111/Sprint 4/day 3 Full Stack CRUD 2/frontend/src/components/Notes.jsx
@@ -4,21 +4,23 @@ const Notes = () => {
 
     const [notes, setNotes] = useState("")
 
-    // const [title, settitle] = useState("")
-    // const [body , setbody] = useState("")
-    // const [category , setcategory] = useState("")
+    const [title, settitle] = useState("")
+    const [body , setbody] = useState("")
+    const [category , setcategory] = useState("")
+    const [author , setauthor] = useState("")
+    const [noteID, setnoteID] = useState("")
 
 
     useEffect(()=>{
-      const auth = localStorage.getItem("token")
-      console.log(auth)
         fetch("http://localhost:8000/notes", {
             headers : { //getting the token from localstorage
-                "Authorizartion" : `Bearer ${localStorage.getItem("token")}`
+                "Authorization" : `Bearer ${localStorage.getItem("token")}`
             }
         })
         .then((res)=>res.json())
-        .then((res)=>console.log(res))
+        .then((res)=>{console.log(res)
+            setNotes(res)
+        })
         .catch((err)=>console.log(err))        
     },[])
 
@@ -38,19 +40,48 @@ const Notes = () => {
 //         .catch(err=>console.log(err))   
 // }
 
-// const HandleDelete = ()=>{
-  
-// }
+const HandlePatch = ()=>{
+    // const data = {title, body, category, author}
+    
 
-//   return (
-//     <div>
-//       <input type="text" placeholder='Title' onChange={(e)=>settitle(e.target.value)}/>
-//       <input type="text" placeholder='Body' onChange={(e)=>setbody(e.target.value)}/>
-//       <input type="text" placeholder='Category' onChange={(e)=>setcategory(e.target.value)}/>
-//       <button type="submit" onClick={HandleCreate}>Create Note</button>
-//       <button type="submit" onClick={HandleDelete}>Delete Note</button>
-//     </div>
-//   )
+    // fetch("http://localhost:8000/notes/update/notes",{
+    //     method : "PATCH",
+    //     headers : {
+    //         "Authorization" : `Bearer ${localStorage.getItem("token")}`,
+    //         "Content-type" : "application/json"
+    //     },
+    //     body : JSON.stringify(data)
+    // })
+    // .then(res=>res.json())
+    // .then(res=>{console.log(res)})      
+    // .catch(err=>console.log(err)) 
+}
+
+
+
+return (
+    <>
+    <h2>Notes</h2>
+    {notes.length===0? <h3>This user has no notes</h3> :  
+    
+        notes.map((item) => {
+            return<>
+            <input value={item.title} placeholder='Title' onChange={(e)=>settitle(e.target.value)} />
+            <input value={item.body} placeholder='body' onChange={(e)=>setbody(e.target.value)} />
+            <input value={item.author} placeholder='author' onChange={(e)=>setauthor(e.target.value)}/>
+            <input value={item.category} placeholder='category' onChange={(e)=>setcategory(e.target.value)}/>    
+            <button onClick={HandlePatch}>Update</button>
+            <button>Delete</button>
+            </>
+            
+        })
+    }
+     
+    
+   
+    </>
+    
+)
 }
 
 export default Notes
